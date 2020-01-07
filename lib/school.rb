899ec830278bc19434bc501f8ce5ee13b4899c64
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-01-07 20:18:36 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+class School
+
+  def initialize(roster)
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if roster[grade]
+      roster[grade].push(name)
+    else
+      roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    roster.each do |k, v|
+      if grade == k
+        return v
+      end
+    end
+  end
+
+  def sort
+    new_hash = {}
+    roster.each do |k, v|
+      new_hash[k] = v.sort
+    end
+
+    new_hash
+  end
+
+
+end
